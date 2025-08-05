b7cab286ec763defe1ab08c871d6a9ca78bb058b
Snealthy <sethn049@gmail.com>
2025-08-04 23:02:07 -0400

    Update orf_finder_skeleton.py

--- starter_code/orf_finder_skeleton.py
+++ starter_code/orf_finder_skeleton.py
@@ -220,7 +220,7 @@ def main():
     		frames   = stored_complete_fra[h_key]   
    		stored_len_fra = [lengths, frames]
 
-		OUTPUT_VIS_FILE = f"{h_key}_orf_lengths.png"
+		OUTPUT_VIS_FILE = f"/output/visualization{h_key}_orf_lengths.png"
 
                 create_visualization(stored_len_fra, OUTPUT_VIS_FILE)
 
